fc16e1c3637139ab29f4631de163a42e4c1f7923
jrks1998 <jonasr.k.s1998@gmail.com>
2017-09-22 19:30:38 -0300

    aula 2

---
+++ python2-entrada-e-saida.py
@@ -0,0 +1,15 @@
+# exercicio
+nome = input('seu nome: ')
+cpf = input('sey cpf: ')
+endereco = input('seu endereço: ')
+idade = input('sua idade: ')
+altura = input('sua altura: ')
+tel = input('seu telefone: ')
+
+print('\n\nseus dados')
+print('nome:', nome)
+print('cpf:', cpf)
+print('endereço:', endereco)
+print('idade:', idade)
+print('altura', altura)
+print('telefone', tel)
